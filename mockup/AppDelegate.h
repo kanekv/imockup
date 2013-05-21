2b0148610bf88dd1523c2515e35bbf601ba68282
Kane <kane@sengrid.com>
2013-05-21 12:47:05 -0700

    Initial Commit

---
+++ mockup/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  mockup
+//
+//  Created by Kane on 5/21/13.
+//  Copyright (c) 2013 Sendgrid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ mockup/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  mockup
+//
+//  Created by Kane on 5/21/13.
+//  Copyright (c) 2013 Sendgrid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
